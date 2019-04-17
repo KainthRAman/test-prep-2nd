776469400334b9adee17433ac8a8554293407527
visualstudio <visualstudio@122-PC34>
2019-04-17 09:11:54 -0400

    second commit

--- test prep 2nd/Program.cs
+++ test prep 2nd/Program.cs
@@ -10,6 +10,16 @@ namespace test_prep_2nd
     {
         static void Main(string[] args)
         {
+            Program a = new Program();
+            Console.WriteLine("the longest word is:");
+            Console.WriteLine(a.MyFunctionA("Ada Lovelace wrote the first algorithem designed for processing by an Analytic Engine."));
+        }
+        public MyFunctionA(string input)
+        {
+            //Write a C# program to find the longest word in a string.
+            string[] words = input.Split('');
+            int
+        }
 
         }
     }
